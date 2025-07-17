0d5783987e5f24079ee18996d7842f56d946329d
Maciej Morawski <m.morawski003@gmail.com>
2025-07-17 23:34:06 +0200

    Update Najczęściej_pomijane_utwory.sql

--- Queries/Najczęściej_pomijane_utwory.sql
+++ Queries/Najczęściej_pomijane_utwory.sql
@@ -1,4 +1,4 @@
 select artist_name, track_name,
-count(track_name) as liczba_skipów
+count(track_name) as suma_skipow
 from spotify_history where skipped ='true'
-group by track_name, artist_name order by liczba_skipów desc;
+group by track_name, artist_name order by suma_skipow desc;
