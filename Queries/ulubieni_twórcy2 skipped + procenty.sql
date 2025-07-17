7124ebc4a122b48ee9cb5c1417dfe1e2167f8d99
Maciej Morawski <m.morawski003@gmail.com>
2025-07-17 23:37:40 +0200

    Update ulubieni_twórcy2 skipped + procenty.sql

--- Queries/ulubieni_twórcy2 skipped + procenty.sql
+++ Queries/ulubieni_twórcy2 skipped + procenty.sql
@@ -1,6 +1,6 @@
 select artist_name, 
-count(case when skipped ='false' then 1 end) as liczba_odtworzeñ,
-count(case when skipped ='true' then 1 end) as liczba_pominiêæ,
+count(case when skipped ='false' then 1 end) as liczba_odtworzen,
+count(case when skipped ='true' then 1 end) as liczba_pominiec,
 case when count(case when skipped ='false' then 1 end) < count(case when skipped ='true' then 1 end)
 then
 round(cast(count(case when skipped ='false' then 1 end)as float)
@@ -9,4 +9,4 @@ else
 round(cast(count(case when skipped ='true' then 1 end)as float)
 /nullif(count(case when skipped ='false' then 1 end),0)*100, 2) end as procent_pomijanych
 from spotify_history 
-group by artist_name order by liczba_odtworzeñ desc;
+group by artist_name order by liczba_odtworzen desc;
