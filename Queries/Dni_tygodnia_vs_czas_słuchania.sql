c557af98336befd29c341b5d2249402228c3d686
Maciej Morawski <m.morawski003@gmail.com>
2025-07-17 23:31:57 +0200

    Update Dni_tygodnia_vs_czas_słuchania.sql

--- Queries/Dni_tygodnia_vs_czas_słuchania.sql
+++ Queries/Dni_tygodnia_vs_czas_słuchania.sql
@@ -2,5 +2,5 @@ set datefirst 1;
 
 select datepart(weekday,ts) as dzien_tygodnia,
 datename(weekday,ts) as nazwa_dnia,
-sum(ms_played/60000) as przes³chane_minuty from spotify_history 
+sum(ms_played/60000) as przesluchane_minuty from spotify_history 
 group by datename(weekday,ts), datepart(weekday,ts) order by dzien_tygodnia;
