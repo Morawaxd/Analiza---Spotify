5e9939616a752e944c595836dc9b79c9cae2081a
Maciej Morawski <m.morawski003@gmail.com>
2025-07-17 23:35:23 +0200

    Update Roczna_liczba_odsłuchów.sql

--- Queries/Roczna_liczba_odsłuchów.sql
+++ Queries/Roczna_liczba_odsłuchów.sql
@@ -1,2 +1,2 @@
-select distinct(year(ts)) as year, count(year(ts)) as liczba_przes³uchañ from spotify_history 
+select distinct(year(ts)) as year, count(year(ts)) as liczba_odtworzen from spotify_history 
 group by year(ts) order by year;
