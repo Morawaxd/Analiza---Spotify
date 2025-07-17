1cf06dfc81538c3afad39411a60a56eeb54f564e
Maciej Morawski <m.morawski003@gmail.com>
2025-07-17 23:32:30 +0200

    Update Dzienny_czas_słuchania_w_minutach.sql

--- Queries/Dzienny_czas_słuchania_w_minutach.sql
+++ Queries/Dzienny_czas_słuchania_w_minutach.sql
@@ -1,4 +1,4 @@
 select year(ts) as year,
-cast(sum(ms_played/60000)/365 as float) as 'dzienny_œredni_czas_s³uchania(min)'
+cast(sum(ms_played/60000)/365 as float) as 'dzienny_sredni_czas_sluchania(min)'
 from spotify_history
-group by year(ts) order by 'dzienny_œredni_czas_s³uchania(min)' desc
+group by year(ts) order by 'dzienny_sredni_czas_sluchania(min)' desc
