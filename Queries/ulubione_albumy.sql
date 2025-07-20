947eeb816ce1f4df15efb9980ad77374305ed648
Maciej Morawski <m.morawski003@gmail.com>
2025-07-20 15:05:26 +0200

    Update ulubione_albumy.sql

--- Queries/ulubione_albumy.sql
+++ Queries/ulubione_albumy.sql
@@ -1,2 +1,2 @@
-select top 20 artist_name,count(track_name) as liczba_odtworzen, album_name from spotify_history
+select top 500 artist_name,count(track_name) as liczba_odtworzen, album_name from spotify_history
 group by album_name, artist_name order by liczba_odtworzen desc;
