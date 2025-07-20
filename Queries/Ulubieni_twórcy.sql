a6aeabb293437c281ca239ae4bb94ded4caefd03
Maciej Morawski <m.morawski003@gmail.com>
2025-07-20 16:16:40 +0200

    Update Ulubieni_twórcy.sql

--- Queries/Ulubieni_twórcy.sql
+++ Queries/Ulubieni_twórcy.sql
@@ -1,3 +1,3 @@
-select top 10 artist_name, count(artist_name) as liczba_odtworzen
+select top 20 artist_name, count(artist_name) as liczba_odtworzen
 from spotify_history group by artist_name	
 order by count(artist_name) desc;
