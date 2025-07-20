fbef8d4b53a96f73a6d95e37b19092e1db382538
Maciej Morawski <m.morawski003@gmail.com>
2025-07-20 16:17:30 +0200

    Update ulubieni_twórcy2 skipped + procenty.sql

--- Queries/ulubieni_twórcy2 skipped + procenty.sql
+++ Queries/ulubieni_twórcy2 skipped + procenty.sql
@@ -3,8 +3,8 @@ count(case when skipped ='false' then 1 end) as liczba_odtworzen,
 count(case when skipped ='true' then 1 end) as liczba_pominiec,
 case when count(case when skipped ='false' then 1 end) < count(case when skipped ='true' then 1 end)
 then
-round(cast(count(case when skipped ='false' then 1 end)as float)
-/nullif(count(case when skipped ='true' then 1 end),0)*100,2)
+ABS(round(cast(count(case when skipped ='false' then 1 end)as float)
+/nullif(count(case when skipped ='true' then 1 end),0)*100,2) - 100)
 else
 round(cast(count(case when skipped ='true' then 1 end)as float)
 /nullif(count(case when skipped ='false' then 1 end),0)*100, 2) end as procent_pomijanych
